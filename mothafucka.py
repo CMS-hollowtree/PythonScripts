3d89dfb461f2832d1ef2e13a6c48f990d67e0b84
Conman1136 <sullivanc28@me.com>
2016-09-06 16:09:24 -0400

    Added 2 files

---
+++ mothafucka.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from random import choice
+
+mothas = [
+	'Surprise,',
+	'Some Fries,',
+	'Damn Flies,',
+	'Supplies,',
+	'Bowties,',
+	'Sunrise,',
+	'Bee Hives,',
+	'Baptize,',
+	'All Rise,',
+	'Revise,',
+	'Hillary Lies,',
+	'Some Pies,',
+	'Cats Eyes,',
+	'Disguise,'
+]
+
+print(choice(mothas), 'Mothafucka')
